b4e52dd6bb8f0de211a707993d59648300d22be5
Derek Surguy <Surguy.Derek@gmail.com>
2024-08-04 15:04:47 -0500

    Use anyhow and anyhow_tauri to make command life easier

--- src-tauri/src/asset.rs
+++ src-tauri/src/asset.rs
@@ -19,142 +19,108 @@ pub mod commands {
     use std::sync::Mutex;
     use uuid::Uuid;
     use super::{Asset, AssetFile};
-    use crate::invoke::InvokeError;
     use crate::database::DatabaseState;
+    use anyhow_tauri;
+    use anyhow;
 
-    #[tauri::command]
-    pub fn create_asset(state: tauri::State<Mutex<DatabaseState>>, name: &str) -> String {
+    fn _create_asset(state: tauri::State<Mutex<DatabaseState>>, name: &str) -> anyhow::Result<String> {
         let uuid = Uuid::new_v4();
         let formatted_uuid = format!("{}", uuid.as_hyphenated());
         let connection = &state.lock().unwrap().connection;
         connection.execute(
             "INSERT INTO asset (name, uuid) VALUES (?1, ?2)",
             (name, format!("{}", formatted_uuid)),
-        ).expect("Unable to insert new asset");
-        return formatted_uuid;
+        )?;
+        return Ok(formatted_uuid);
     }
 
     #[tauri::command]
-    pub fn list_assets(state: tauri::State<Mutex<DatabaseState>>) -> Vec<Asset> {
+    pub fn create_asset(state: tauri::State<Mutex<DatabaseState>>, name: &str) -> anyhow_tauri::TAResult<String> {
+        let out = _create_asset(state, name)?;
+        return Ok(out);
+    }
+
+    fn _list_assets(state: tauri::State<Mutex<DatabaseState>>) -> anyhow::Result<Vec<Asset>> {
         let connection = &state.lock().unwrap().connection;
         let mut stmt = connection.prepare("SELECT rowid, name, uuid FROM asset LIMIT 100").expect("Unable to prepare list_assets SELECT");
         let rows = stmt.query_map((), |row| {
-            let name: String = row.get::<usize, String>(1).expect("list_assets::Unable to retrieve name from row");
-            let uuid: String = row.get::<usize, String>(2).expect("list_assets::Unable to retrieve uuid from row");
+            let name: String = row.get::<usize, String>(1)?;
+            let uuid: String = row.get::<usize, String>(2)?;
 
             Ok(Asset {
                 name,
                 uuid
             })
-        }).expect("Unable to execute list_assets query");
+        })?;
         
         let mut output = Vec::new();
         for asset in rows {
-            output.push(asset.expect("asset"));
+            output.push(asset?);
         }
-        return output;
+        return Ok(output);
     }
 
     #[tauri::command]
-    pub fn get_asset(state: tauri::State<Mutex<DatabaseState>>, uuid: &str) -> Result<Asset, InvokeError> {
+    pub fn list_assets(state: tauri::State<Mutex<DatabaseState>>) -> anyhow_tauri::TAResult<Vec<Asset>> {
+        let out = _list_assets(state)?;
+        return Ok(out);
+    }
+
+    fn _get_asset(state: tauri::State<Mutex<DatabaseState>>, uuid: &str) -> anyhow::Result<Asset> {
         let connection = &state.lock().unwrap().connection;
-        let mut stmt = connection.prepare("SELECT rowid, name, uuid FROM asset WHERE uuid = ?1").expect("Unable to prepare list_assets SELECT");
-        let wrapped_rows = stmt.query_map([uuid], |row| {
-            let name: String = row.get::<usize, String>(1).expect("list_assets::Unable to retrieve name from row");
-            let uuid: String = row.get::<usize, String>(2).expect("list_assets::Unable to retrieve uuid from row");
+        let mut stmt = connection.prepare("SELECT rowid, name, uuid FROM asset WHERE uuid = ?1")?;
+        let mut rows = stmt.query_map([uuid], |row| {
+            let name: String = row.get::<usize, String>(1)?;
+            let uuid: String = row.get::<usize, String>(2)?;
 
             Ok(Asset {
                 name,
                 uuid
             })
-        });
-
-        let mut rows = match wrapped_rows {
-            Err(_e) => return Err(InvokeError {
-                message: "Unable to execute get_asset query".into(),
-                status: "500".into()
-            }),
-            Ok(asset) => asset
-        };
+        })?;
         
-        let asset_result = match rows.next() {
-            None => return Err(InvokeError {
-                message: "No assets found matching uuid".into(),
-                status: "404".into()
-            }),
-            Some(v) => v
-        };
+        let asset = rows.next().unwrap()?;
 
-        let asset = match asset_result {
-            Err(_e) => return Err(InvokeError {
-                message: "Error unwrapping asset result".into(),
-                status: "500".into()
-            }),
-            Ok(val) => val
-        };
-        
         return Ok(asset);
     }
-
+    
     #[tauri::command]
-    pub fn add_file_to_asset(
+    pub fn get_asset(state: tauri::State<Mutex<DatabaseState>>, uuid: &str) -> anyhow_tauri::TAResult<Asset> {
+        let out = _get_asset(state, uuid)?;
+        
+        return Ok(out);
+    }
+
+    fn _add_file_to_asset(
         app_handle: tauri::AppHandle,
         _state: tauri::State<Mutex<DatabaseState>>,
         asset_uuid: &str,
         name: &str,
         description: &str,
         file_path: &str
-    ) -> Result<AssetFile, InvokeError> {
+    ) -> anyhow::Result<AssetFile> {
         let uuid = Uuid::new_v4().to_string();
         
         // TODO: copy file from path into vault
-        let data_dir = app_handle.path_resolver().app_data_dir().expect("Unable to retrieve data dir");
+        let data_dir = app_handle.path_resolver().app_data_dir().unwrap();
         
         // Create asset dir if not exists
-        create_dir_all(data_dir.join(format!("assets/{asset_uuid}"))).expect("Unable to create asset directory");
+        create_dir_all(data_dir.join(format!("assets/{asset_uuid}")))?;
         
         // Verify file at file_path exists
-        let file_meta = match metadata(file_path) {
-            Err(_e) => return Err(InvokeError {
-                message: "Unable to read metadata of file at file_path".into(),
-                status: "400".into()
-            }),
-            Ok(v) => v
-        };
+        let file_meta = metadata(file_path)?;
 
         if file_meta.is_file() == false {
-            return Err(InvokeError {
-                message: "Given path is not a file".into(),
-                status: "400".into()
-            });
+            return Err(anyhow::anyhow!("Not file"));
         }
 
         // Get extension from file_path
-        let extension = match Path::new(file_path).extension() {
-            None => return Err(InvokeError {
-                message: "Unable to get extension from file".into(),
-                status: "500".into()
-            }),
-            Some(v) => match v.to_str() {
-                None => return Err(InvokeError {
-                    message: "Unable to get extension from file".into(),
-                    status: "500".into()
-                }),
-                Some(v) => v 
-            }
-        };
+        let extension = Path::new(file_path).extension().ok_or(anyhow::anyhow!("Unable to get extension"))?.to_str().ok_or(anyhow::anyhow!("Unable to convert extension to string"))?;
 
         // Copy the file to uuid.<ext>
         let target_file = format!("assets/{asset_uuid}/{uuid}.{extension}");
-        let copy_result = copy(file_path, data_dir.join(target_file));
+        copy(file_path, data_dir.join(target_file))?;
 
-        match copy_result {
-            Err(_e) => return Err(InvokeError {
-                message: "Unable to get extension from file".into(),
-                status: "500".into()
-            }),
-            Ok(_v) => {}
-        }
         // TODO: Add file to DB
 
         // TODO: Add entry to intersection table
@@ -164,5 +130,18 @@ pub mod commands {
             name: name.to_string(),
             description: description.to_string()
         });
+    }
+
+    #[tauri::command]
+    pub fn add_file_to_asset(
+        app_handle: tauri::AppHandle,
+        state: tauri::State<Mutex<DatabaseState>>,
+        asset_uuid: &str,
+        name: &str,
+        description: &str,
+        file_path: &str
+    ) -> anyhow_tauri::TAResult<AssetFile> {
+        let out = _add_file_to_asset(app_handle, state, asset_uuid, name, description, file_path)?;
+        return Ok(out);
     }
 }
