c30ee0f5e5529f5714dc0db819d1ee661013d163
Derek Surguy <Surguy.Derek@gmail.com>
2024-08-17 11:17:56 -0500

    Adjust database schema for better constraints; Create seed data

--- src-tauri/resources/sqlite-setup.sql
+++ src-tauri/resources/sqlite-setup.sql
@@ -1,5 +1,5 @@
 CREATE TABLE asset (
-    uuid TEXT UNIQUE ON CONFLICT ABORT NOT NULL,
+    uuid TEXT UNIQUE ON CONFLICT ROLLBACK NOT NULL,
     name TEXT NOT NULL,
     last_update TEXT
 );
@@ -7,18 +7,19 @@ CREATE TABLE asset (
 CREATE VIRTUAL TABLE asset_fts using fts5(uuid, name, content=asset, content_rowid=rowid);
 
 CREATE TABLE tag (
-    text TEXT NOT NULL
+    text TEXT UNIQUE ON CONFLICT ROLLBACK NOT NULL 
 );
 
 CREATE VIRTUAL TABLE tag_fts USING fts5(text, content=tag, content_rowid=rowid);
 
 CREATE TABLE tag_to_asset (
+    asset_id REFERENCES asset (rowid),
     tag_id REFERENCES tag (rowid),
-    asset_id REFERENCES asset (rowid)
+    PRIMARY KEY (asset_id, tag_id) ON CONFLICT ROLLBACK
 );
 
 CREATE TABLE asset_file (
-    uuid TEXT UNIQUE ON CONFLICT ABORT NOT NULL,
+    uuid TEXT UNIQUE ON CONFLICT ROLLBACK NOT NULL,
     name TEXT NOT NULL,
     description TEXT NOT NULL,
     extension TEXT NOT NULL,
@@ -27,5 +28,6 @@ CREATE TABLE asset_file (
 
 CREATE TABLE asset_to_asset_file (
     asset_id REFERENCES asset (uuid),
-    asset_file_id REFERENCES asset_file (uuid)
+    asset_file_id REFERENCES asset_file (uuid),
+    PRIMARY KEY (asset_id, asset_file_id) ON CONFLICT ROLLBACK
 );
